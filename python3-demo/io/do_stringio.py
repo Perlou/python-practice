6067e0f3ccc1661f97612403fa8a6e2a6a7c88eb
Perlou <a87387875@163.com>
2017-06-15 17:40:24 +0800

    feat io

---
+++ python3-demo/io/do_stringio.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from io import StringIO
+
+f = StringIO()
+f.write('hello')
+f.write(' ')
+f.write('world!')
+print(f.getvalue())
+
+f = StringIO('水面细风生，\n菱歌慢慢声。\n客亭临小市，\n灯火夜妆明。')
+while True:
+    s = f.readline()
+    if s == '':
+        break
+    # print(s)
+    print(s.strip())
